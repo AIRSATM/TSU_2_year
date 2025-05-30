7ab9202833edaaf428c10f430d5867423fa7c9ce
AIRSATM <157048934+AIRSATM@users.noreply.github.com>
2025-05-30 20:34:34 +0700

    Update Big_Number.cpp

--- Big_Number.cpp
+++ Big_Number.cpp
@@ -4,8 +4,10 @@
 #include <ctime>
 #include <cmath>
 #include <cctype>
+#include <iomanip>
+#include <vector>
 
-#define BASE_SIZE (sizeof(BASE)*8)
+#define BASE_SIZE (sizeof(BASE)*8) 
 
 using namespace std;
 
@@ -14,145 +16,148 @@ typedef unsigned short int BASE;
 
 class BN {
 private:
-    int maxlen;
-    int len;
-    BASE *coef;
+    int maxlen;     
+    int len;        
+    BASE *coef;     
+
 public:
-    // конструктор по умолчанию
+    
     BN() : maxlen(1), len(1) {
         coef = new BASE[maxlen];
-        coef[0] = 0;
+        // coef[0] = 0;
         cout << coef[0];
     }
 
-    // конструктор 2 + конструктор 3 (заполняем нулями и заполняем числами случайными)
     BN(int maximal, bool randomize): maxlen(maximal), len(maximal) {
         coef = new BASE[maximal];
-        int size = BASE_SIZE;
-        maxlen = maximal;
-        len = maximal;
-        
+        int size = BASE_SIZE; 
+
         if (randomize) {
-            int parts = (BASE_SIZE + 3) /4; // храним цифры по частям в динамическом массиве
+            int parts = (BASE_SIZE)/4; 
             for (int i = 0; i < maxlen; i++) {
                 BASE value = 0;
                 for (int j = 1; j < parts; j++) {
-                    BASE hex_digit = rand() % 16;
-                   value |= (hex_digit << (j*4));
+                    BASE hex_digit = rand() % 16; 
+                    value |= (hex_digit << (j*4)); 
                 } 
-            coef[i] = value;
-            cout << coef[i] << " ";
+                coef[i] = value; 
+                cout << coef[i] << " "; 
             }
-            
-            // убираем ведущие нули
+
             len = maxlen;
             while (len > 1 && coef[len - 1] == 0)
                 len--;
         } else {
+            
+            fill(coef, coef + maxlen, 0);
+            len = 1;
             for (int i = 0; i < maxlen; i++) {
-                coef[i] = 0;
                 cout << coef[i] << " ";
             }
-            len = 1; // обновляем, чтобы не было пустого вывода
         }
+        cout << dec << endl; 
     }
-    // конструктор копирования
+
+    
     BN(const BN& other): maxlen(other.maxlen), len(other.len) {
         coef = new BASE[maxlen];
-        memcpy(coef,other.coef,maxlen*sizeof(BASE));
+        memcpy(coef, other.coef, maxlen * sizeof(BASE)); 
     }
-    // деструктор
+
+   
     ~BN() {
         delete[] coef;
         coef = NULL;
     }
-    // оператор =
-    BN& operator=(const BN& other){
-        // if (this != &other){ // сразу проверка на самоприсваивание
-        //     // освобождаем старые ресурсы
-        //     delete[] coef;
-            
-        //     // создаем временные копии
-        //     BASE *new_coef = new BASE[other.maxlen];
-        //     memcpy(new_coef,other.coef,other.maxlen*sizeof(BASE));
 
-        //     // обновляем состояние
-        //     maxlen = other.maxlen;
-        //     len = other.len;
-        //     coef = new_coef;
-        // }
-        // return *this;
-    }
-    // ввод в hex
-    friend istream& operator >> (istream& is, BN& b_number) {
-        // char* s = new char[1000];
-        
-        // int j = 0;
-        // int k = 0;
-        
-        // unsigned int tmp = 0;
-        // is.getline(s,1000);
-        
-        // b_number.len = (strlen(s) - 1)/((BASE_SIZE/4) + 1);
-        // b_number.maxlen = b_number.len;
-        // b_number.coef = new BASE[b_number.maxlen];
-        // for (int i = 0; i < b_number.len; i++){
-        //     b_number.coef[i] = 0;
-        // }
-        
-        // for (int i = strlen(s) - 1; i > -1; i--){
-        //     if (s[i] >= '0' && s[i] <= '9')
-        //         tmp = s[i] - '0';
-        //     else if (s[i] >= 'a' && s[i] <= 'f')
-        //         tmp = s[i] - 'a' + 10;
-        //     else if (s[i] >= 'A' && s[i] <= 'F')
-        //         tmp = s[i] - 'A' + 10;
-        //     else
-        //         throw invalid_argument("Инвалид!!!");
-        // }
-        // b_number.coef[j] |= tmp << k;
-        // k += 4;
-        // if (k >= BASE_SIZE){
-        //     k = 0;
-        //     j++;
-        // }
-        // return is;
-    }
-    // вывод в hex
-    friend ostream& operator << (ostream& os, BN& number) {
-        // int size = BASE_SIZE;
-        // char* s = new char[1000];
-        // unsigned int tmp = 0;
-        // for (int i = 0; i < number.len - 1; i++){
-        //     for (int j = number.len - 1; j > -1; j--){
-        //         int k = BASE_SIZE - 4;
-        //         tmp = (number.coef[j] >> k)&(0xf);
-        //         if (0 <= tmp && tmp <= 9)
-        //             s[i] = (char)(tmp + '0');
-        //         if (10 <= tmp <= 15)
-        //             s[i] = (char)(tmp - 10 + 'a');
-        //         k-= 4;
-        //         if (k < 0)
-        //             k = BASE_SIZE - 4;
-        //     }
-        // }
-        // return os;
-    }
     
+    BN& operator=(const BN& other){
+        if (this != &other){ 
+            delete[] coef;
+            BASE *new_coef = new BASE[other.maxlen];
+            memcpy(new_coef, other.coef, other.maxlen * sizeof(BASE));
+
+            maxlen = other.maxlen;
+            len = other.len;
+            coef = new_coef;
+        }
+        return *this;
+    }
+
+    friend istream& operator>>(istream& is, const BN& num) {
+        string s;
+        is >> s;
+        int len = (s.length()-1)/(BASE_SIZE/4)+1;
+        int maxlen = len;
+        
+        // delete[] num.coef;
+        
+        
+        // fill(num.coef, num.coef + maxlen,0);
+        
+        int j = 0;
+        int k = 0;
+        int tmp = 0;
+        for (int i = s.length()-1; i >= 0; i--){
+            if ('0' <= s[i] && s[i] <= '9')
+                tmp |= (s[i] - '0')<<k;
+            else if ('a' <= s[i] && s[i] <= 'f')
+                tmp |= (s[i] - 'a' + 10)<<k;
+            else if ('A' <= s[i] && s[i] <= 'F')
+                tmp |= (s[i] - 'A' + 10)<<k;
+            else {
+                cerr << "Error" << s[i];
+            }
+            k += 4;
+            if (k >= BASE_SIZE) {
+                num.coef[j++] = tmp;
+                tmp = 0;
+                k = 0;
+            }
+        }
+        if (k > 0) {
+            num.coef[j] = tmp;
+        }
+        return is;
+    }
+
+    friend ostream& operator<<(ostream& os, const BN& num) {
+        os << "BN: ";
+        for (int i = 0; i <= num.len - 1; i++) {
+            // os << hex << setw(4) << setfill('0') << num.coef[i] << " ";
+            os.width(BASE_SIZE/4);
+            os.fill('0');
+            os << hex << num.coef[i] << " ";
+        }
+        return os;
+    }
 };
 
 int main()
 {
-    srand(time(0));
+    srand(time(0)); 
+
+    cout << "конструктор 1: ";
     BN a1;
     cout << "\n";
-    BN a2(100,1);
-    cout << "\n";
-    BN a3;
-    cout << "\n";
-    // cin >> a3;
-    // cout << a3 << endl;
+
+    cout << "рандом-конструктор 2: ";
+    BN a2(12, 1); 
+
+    cout << "конструктор копирования: \n";
+    BN a3(a2); 
+    cout << a3; 
+
+    cout << "\n оператор присваивания: \n";
+    BN a4;
+    a4 = a2; 
+    cout << a4; 
     
+    cout << "\n HEX-ввод: \n";
+    BN a5;
+    cin >> a5;
     
+    cout << "Hex-вывод: " << a5;
+
     return 0;
 }
